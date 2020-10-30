0b483b8372eafe6b2de15bd7538aac1fec5f7d6d
Damien de la Bruère-Terreault <damiendlbt@gmail.com>
2020-10-30 15:03:28 -0400

    Création d'un GamePad

---
+++ src/cegepst/TankGame.java
@@ -0,0 +1,27 @@
+package cegepst;
+
+import cegepst.engine.Buffer;
+import cegepst.engine.Game;
+
+public class TankGame extends Game {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void conclude() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Buffer buffer) {
+
+    }
+}
